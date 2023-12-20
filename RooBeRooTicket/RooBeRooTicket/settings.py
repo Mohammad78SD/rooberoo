feeb40789b93e7b7c35003c1c8754ca9838ef044
MohammadSD <mohammadsd2000@gmail.com>
2023-12-20 19:41:16 +0330

    feat: create User app classes and add it to django admin

--- RooBeRooTicket/RooBeRooTicket/settings.py
+++ RooBeRooTicket/RooBeRooTicket/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'User',
+    'Event',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'RooBeRooTicket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'RooBeRoo_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Mohammad.2000',
+        'HOST': 'localhost',  # Change as per your PostgreSQL configuration
+        'PORT': '5432',           # Change as per your PostgreSQL configuration
     }
 }
 
--- RooBeRooTicket/User/models.py
+++ RooBeRooTicket/User/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django_jalali.db import models as jmodels
+
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone_number = models.CharField(
+            max_length=15,
+            validators=[
+                RegexValidator(
+                    regex=r"^\+\d{1,3}\d{3}\d{3}\d{4}$",
+                    message="Phone number must be in the format +XXX-XXX-XXXX.",
+                )
+            ],
+            unique=True,
+        )
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=255)
+    date_of_birth = jmodels.jDateTimeField()
+    # membership = models.ManyToManyField('Membership', through='UserMembership')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
