9eb0fd18cfacd59a3f3bed0d88cc69ba28bc592f
MohammadSD <mohammadsd2000@gmail.com>
2023-12-20 20:09:55 +0330

    feat: created events classes and add it to django admin

--- RooBeRooTicket/Event/admin.py
+++ RooBeRooTicket/Event/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ejra, rooydad, sans, review
+
 
 # Register your models here.
+admin.site.register(ejra)
+admin.site.register(rooydad)
+admin.site.register(sans)
+admin.site.register(review)
--- RooBeRooTicket/Event/models.py
+++ RooBeRooTicket/Event/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django_jalali.db import models as jmodels
+from User.models import User
+
 
 # Create your models here.
+class rooydad(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    # banner = models.ImageField(upload_to='images/')
+    # photos_gallery
+    # video_teaser = models.FileField(upload_to='videos/')
+
+class ejra(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    capacity = models.IntegerField()
+    free_capacity = models.IntegerField()
+    min_ticket = models.IntegerField()
+    max_ticket = models.IntegerField()
+    start_sale_date = jmodels.jDateTimeField()
+    end_sale_date = jmodels.jDateTimeField()
+    description = models.TextField()
+    rooydad = models.ManyToManyField(rooydad)
+    
+class sans(models.Model):
+    date = jmodels.jDateTimeField()
+    ejra = models.ManyToManyField(ejra)
+    
+class review(models.Model):
+    review_text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rooydad = models.ForeignKey(rooydad, on_delete=models.CASCADE)
+    
+
+    
