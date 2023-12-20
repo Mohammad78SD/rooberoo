c8f5516457832590c170ab7f45f4196960dc9590
MohammadSD <mohammadsd2000@gmail.com>
2023-12-20 21:17:57 +0330

    feat: admin panel searc and filter

--- RooBeRooTicket/Event/admin.py
+++ RooBeRooTicket/Event/admin.py
@@ -3,7 +3,11 @@ from .models import ejra, rooydad, sans, review
 
 
 # Register your models here.
-admin.site.register(ejra)
+class ejraAdmin(admin.ModelAdmin):
+    search_fields = ['title',]
+    list_filter = ['start_sale_date',]
+    
+admin.site.register(ejra, ejraAdmin)
 admin.site.register(rooydad)
 admin.site.register(sans)
 admin.site.register(review)
--- RooBeRooTicket/Event/models.py
+++ RooBeRooTicket/Event/models.py
@@ -11,8 +11,14 @@ class rooydad(models.Model):
     # banner = models.ImageField(upload_to='images/')
     # photos_gallery
     # video_teaser = models.FileField(upload_to='videos/')
+    
+    class Meta:
+        verbose_name = "1. رویداد ها"
+    def __str__(self) -> str:
+        return self.title
 
 class ejra(models.Model):
+    title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     capacity = models.IntegerField()
     free_capacity = models.IntegerField()
@@ -21,16 +27,28 @@ class ejra(models.Model):
     start_sale_date = jmodels.jDateTimeField()
     end_sale_date = jmodels.jDateTimeField()
     description = models.TextField()
-    rooydad = models.ManyToManyField(rooydad)
-    
+    rooydad = models.ForeignKey(rooydad, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "2. اجرا ها"
+    def __str__(self) -> str:
+        return self.title
 class sans(models.Model):
     date = jmodels.jDateTimeField()
-    ejra = models.ManyToManyField(ejra)
+    ejra = models.ForeignKey(ejra, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return str(self.date)
+    class Meta:
+        verbose_name = "3. سانس ها"
     
 class review(models.Model):
     review_text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rooydad = models.ForeignKey(rooydad, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "نظرات"
+    
+    def __str__(self) -> str:
+        return (self.user.name + self.rooydad.title)
     
 
     
