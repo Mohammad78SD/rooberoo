f741a5197ffa87c0fa91ff877d7359ffbdcdf42f
MohammadSD <mohammadsd2000@gmail.com>
2023-12-29 20:28:31 +0330

    update: event details page added

--- RooBeRooTicket/Event/urls.py
+++ RooBeRooTicket/Event/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import event_list
+from .views import event_list, detail
 
 urlpatterns = [
     path("list", event_list, name = "event_list"),
+    path('<str:code>', detail, name='detail'),
 ]
--- RooBeRooTicket/Event/views.py
+++ RooBeRooTicket/Event/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Event
+from .models import Event, Sans
 # Create your views here.
 
 
@@ -10,3 +10,45 @@ def event_list(request):
         'events' : events
     }
     return render(request, 'events/eventlist.html', context= eventlist)
+
+
+def detail(request, code):
+    if request.method == 'GET':
+        event = Event.objects.get(pk = code)
+        sans = Sans.objects.filter(event = event)
+
+            
+        context = {
+            'event' : event,
+            'sanslist' : sans, 
+            'flag' : False
+        }
+        return render(request, 'events/details.html', context=context)
+    # if request.method == 'POST':
+    #     current_flight = Flight.objects.get(no=code)
+    #     email=request.POST['email']
+    #     name=request.POST['name']
+    #     lastname=request.POST['lastname']
+    #     nationalid=request.POST['nationalid']
+    #     seat=request.POST['seat']
+    #     # check available seat
+    #     if name == '':
+    #         return HttpResponse('Error: Name should not be empty')
+    #     if int(current_flight.capacity) < int(seat):
+    #         return HttpResponse("Error: There is no enough seat. max seat available is :{}".format(current_flight.capacity))
+    #     Ticket.objects.create(
+    #         flight=current_flight,
+    #         name=name,
+    #         lastname=lastname,
+    #         email = email,
+    #         nationalid=nationalid,
+    #         seat=seat,
+    #         reservation_code=generate_reservation_code()
+    #     )
+    #     current_flight.capacity = current_flight.capacity - int(seat)
+    #     current_flight.save()
+    #     f_list = {
+    #         'flights' : current_flight,
+    #         'flag' : True
+    #     }
+    #     return render(request, 'flights/detail.html', context=f_list)
