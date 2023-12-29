c47f8926d0ca34e85a2fd69ea78c06d1f5446618
MohammadSD <mohammadsd2000@gmail.com>
2023-12-29 19:26:35 +0330

    update: add events list with html render

--- RooBeRooTicket/Event/views.py
+++ RooBeRooTicket/Event/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
 
 
 # define list to render events lists html page
-def list(request):
-    return render(request, 'flights/eventlist.html')
+def event_list(request):
+    events = Event.objects.all()
+    eventlist = {
+        'events' : events
+    }
+    return render(request, 'events/eventlist.html', context= eventlist)
