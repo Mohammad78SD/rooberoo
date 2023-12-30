040b75d624dd8728ec9d0e4b0edc39e715c9f288
MohammadSD <mohammadsd2000@gmail.com>
2023-12-30 18:31:57 +0330

    update: add reserve and generate ticket

--- RooBeRooTicket/Event/views.py
+++ RooBeRooTicket/Event/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Event, Sans
+from django.shortcuts import render, HttpResponse
+from .models import Event, Sans, Ticket, User
 # Create your views here.
 
 
@@ -24,31 +24,26 @@ def detail(request, code):
             'flag' : False
         }
         return render(request, 'events/details.html', context=context)
-    # if request.method == 'POST':
-    #     current_flight = Flight.objects.get(no=code)
-    #     email=request.POST['email']
-    #     name=request.POST['name']
-    #     lastname=request.POST['lastname']
-    #     nationalid=request.POST['nationalid']
-    #     seat=request.POST['seat']
-    #     # check available seat
-    #     if name == '':
-    #         return HttpResponse('Error: Name should not be empty')
-    #     if int(current_flight.capacity) < int(seat):
-    #         return HttpResponse("Error: There is no enough seat. max seat available is :{}".format(current_flight.capacity))
-    #     Ticket.objects.create(
-    #         flight=current_flight,
-    #         name=name,
-    #         lastname=lastname,
-    #         email = email,
-    #         nationalid=nationalid,
-    #         seat=seat,
-    #         reservation_code=generate_reservation_code()
-    #     )
-    #     current_flight.capacity = current_flight.capacity - int(seat)
-    #     current_flight.save()
-    #     f_list = {
-    #         'flights' : current_flight,
-    #         'flag' : True
-    #     }
-    #     return render(request, 'flights/detail.html', context=f_list)
+    
+    
+    if request.method == 'POST':
+        current_event = Event.objects.get(pk = code)
+        user_name = request.POST.get('user_name')
+        user = User.objects.get(name = user_name)
+        quantity = request.POST.get('quantity')
+        sans_id = request.POST.get('sans_id')
+        sans = Sans.objects.get(pk=sans_id)
+        if user == '':
+            return HttpResponse('Error: Name should not be empty')
+        if int(sans.free_capacity) < int(quantity):
+            return HttpResponse("Error: There is no enough seat. max seat available is :{}".format(sans.free_capacity))
+        ticket = Ticket.objects.create(user=user, quantity=quantity, sans=sans)
+        sans.free_capacity -= int(quantity)
+        sans.save()
+        sanses = Sans.objects.filter(event = current_event)
+        context = {
+            'event' : current_event,
+            'sanslist' : sanses, 
+            'flag' : True
+        }
+        return render(request, 'events/details.html', context=context)
