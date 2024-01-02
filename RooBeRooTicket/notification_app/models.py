0ad6e68f2de6147e4c3edc8d156b66938b9673ec
armin <arminjafri138386@gmail.com>
2024-01-02 22:11:14 +0330

    update:models view and create task.py

--- RooBeRooTicket/notification_app/models.py
+++ RooBeRooTicket/notification_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_celery_beat.models import periodicTask,crontabSchedule
 # Create your models here.
+class BroadcastNotification(models.Model):
+    message = models.TextField()
+    broadcast_on = models.DateTimeField()
+    sent = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-broadcast_on']
+
+@receiver(post_save, sender=BroadcastNotification)
+def notification_handler(sender,instance,created,**kwargs):
+    #call group_send function directly to send notifications or you can create a dynamic task in celery beat
+
